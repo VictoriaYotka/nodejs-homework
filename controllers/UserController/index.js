b4995c53bb0b05f3db265b9fa84c92905e0af2b4
VictoriaYotka <victoria.borisyuk@gmail.com>
2023-08-18 14:14:03 +0300

    add users avatars

--- controllers/UserController/index.js
+++ controllers/UserController/index.js
@@ -2,10 +2,12 @@ const registerUser = require('./registerUser')
 const loginUser  =require('./loginUser')
 const getCurrentUser = require('./getCurrentUser')
 const logoutUser = require('./logoutUser')
+const updateAvatar = require('./updateAvatar')
 
 module.exports = {
     registerUser,
     loginUser,
     getCurrentUser,
-    logoutUser
+    logoutUser,
+    updateAvatar
 }
